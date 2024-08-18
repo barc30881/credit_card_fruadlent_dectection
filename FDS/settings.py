b07be5dd6ccb3420c64e18878e1c4f2e8c31e719
barc30881 <barc30881@gmail.com>
2024-08-18 05:24:03 +0100

    Update settings.py

--- FDS/settings.py
+++ FDS/settings.py
@@ -136,4 +136,5 @@ STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static'),
 ]
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
 STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
